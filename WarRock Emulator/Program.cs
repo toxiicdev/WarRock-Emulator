ab0a21b351ff08401ad7a04f408694aa816d6e65
Gennaro <toxiicpp@gmail.com>
2014-11-14 23:38:07 +0100

    Default project

---
+++ WarRock Emulator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WarRock_Emulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
